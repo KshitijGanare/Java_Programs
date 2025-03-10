122450f0eae73db16ecdf5ceb8c44d35e7afb101
Kshitij Ganare <kshitijganare123@gmail.com>
2025-03-10 23:39:49 +0530

    Fifth Commit

---
+++ src/Java_Task_Programs/reverseStringTwo.java
@@ -0,0 +1,17 @@
+package Java_Task_Programs;
+
+import java.util.Scanner;
+
+public class reverseStringTwo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.next();
+
+        String reversed = new StringBuilder(name).reverse().toString();
+
+        System.out.println(reversed);
+
+    }
+}
