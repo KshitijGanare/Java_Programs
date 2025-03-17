e0bf1060a9ebcbf70126f95f0fc1226a8aad5b3d
Kshitij Ganare <kshitijganare123@gmail.com>
2025-03-17 23:37:02 +0530

    Eight Commit

---
+++ src/Java_Task_Programs/ReverseStringThree.java
@@ -0,0 +1,23 @@
+package Java_Task_Programs;
+
+public class ReverseStringThree {
+    public static void main(String[] args) {
+
+        String reverseString = "Kshitij is the Best"; // Reversing the string with words in same position
+
+        String[] storeWords = reverseString.split(" ");
+
+        for (String singleWords : storeWords) {
+
+            char[] lettersPrint = singleWords.toCharArray();
+
+            for (int i = lettersPrint.length-1; i >= 0 ; i--) {
+                System.out.print(lettersPrint[i]);
+            }
+
+            System.out.print(" ");
+
+        }
+
+    }
+}
