12c186a8e89966ea3df059752e0959777de9c405
Kshitij Ganare <kshitijganare123@gmail.com>
2025-05-30 23:16:43 +0530

    New Programs Commit

---
+++ src/String_Programs/AnagramCheckerTwo.java
@@ -0,0 +1,12 @@
+package String_Programs;
+
+public class AnagramCheckerTwo {
+    public static void main(String[] args) {
+
+        String str1 = "12abcd1234#@123";
+        String str2 = "ashitijk2!aaa";
+
+
+
+    }
+}
---
+++ src/String_Programs/CountUnique_Vowel_Consn2.java
@@ -0,0 +1,42 @@
+package String_Programs;
+
+import java.util.Scanner;
+
+public class CountUnique_Vowel_Consn2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+
+        // String: "Flowers are Beautiful", Count unique vowels and consonants in this
+
+        String uniqueVowels = "";
+        String uniqueConsonants = "";
+
+        String lowerCase = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = lowerCase.charAt(i);
+
+            if(ch >= 'a' && ch<= 'z'){
+                if ("aeiou".indexOf(ch) != -1){   // Checks if char equals aeiou
+                    if (uniqueVowels.indexOf(ch) == -1){  // Checks if character present in unique vowel
+                        uniqueVowels += ch; // Added vowels here
+                    }
+                } else if ("aeiou".indexOf(ch) == -1) {
+                    if (uniqueConsonants.indexOf(ch) == -1){
+                        uniqueConsonants += ch;
+                    }
+                }
+            }else {
+                System.out.println("Invalid Character!");
+            }
+
+        }
+
+        System.out.println("Unique Vowels: " + uniqueVowels.length());
+        System.out.println("Unique Consonants: " + uniqueConsonants.length());
+
+
+    }
+}
