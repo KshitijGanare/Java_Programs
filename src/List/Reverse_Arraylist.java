d372a5f4a26c0d207dd197d27fee646309a85277
Kshitij Ganare <kshitijganare123@gmail.com>
2025-06-30 23:09:00 +0530

    New Program Commit

--- src/List/Reverse_Arraylist.java
+++ src/List/Reverse_Arraylist.java
@@ -6,11 +6,12 @@ public class Reverse_Arraylist {
 
     static Reverse_Arraylist ra = new Reverse_Arraylist();
     public static void main(String[] args) {
-        ra.reverse();
+//        ra.reverseByFunction();
+        ra.reverseManual();
     }
 
 
-    public void reverse(){
+    public void reverseByFunction(){
         List<String> arrayList = new ArrayList<>(Arrays.asList("1","2"));
         List<Integer> linkedList = new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
 
@@ -21,6 +22,21 @@ public class Reverse_Arraylist {
         System.out.println(arrayList);
         System.out.println(linkedList);
 
+    }
+
+    public void reverseManual(){
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(10,20,30,40,50));
+
+        int size = list.size();
+
+        for (int i = 0; i < size / 2 ; i++) {
+              int temp = list.get(i);
+              list.set(i, list.get(size - i - 1));
+              list.set((size - i - 1), temp);
+
+        }
+
+        System.out.println("Reversed List: " + list);
     }
 
 }
