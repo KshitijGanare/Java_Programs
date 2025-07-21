3b3bb8617fc73102da246051c9b650cf9897e7df
Kshitij Ganare <kshitijganare123@gmail.com>
2025-07-21 22:21:07 +0530

    New Program Commit

---
+++ src/DSA/Array_Problems/SumOfAllArray.java
@@ -0,0 +1,21 @@
+package DSA.Array_Problems;
+
+import java.util.Arrays;
+
+public class SumOfAllArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,6,8,96,2};
+        String stringArray = Arrays.toString(arr);
+        sumOfAllArray(arr);
+
+    }
+
+    public static void sumOfAllArray(int[] arr){
+        int sum = 0;
+        for (int add: arr){
+            sum = sum+add;
+        }
+        System.out.println(sum);
+    }
+
+}
