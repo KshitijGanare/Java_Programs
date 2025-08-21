5e72c2dc1fe9969f50e3a7f8755df61c7e96996b
Kshitij Ganare <kshitijganare123@gmail.com>
2025-08-21 05:38:42 +0530

    New Programs

---
+++ src/Concepts/encapsulation/Students_Info.java
@@ -0,0 +1,37 @@
+package Concepts.encapsulation;
+
+public class Students_Info {
+
+    // Student Data
+    private String first_name;
+    private String last_name;
+    private int rollno;
+    private int age;
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String firstname){
+        this.first_name = "Booner";
+    }
+
+//    public String getlast_name() {
+//        return last_name;
+//    }
+//
+//    public void setLast_name(String last_name) {
+//        this.last_name = last_name;
+//    }
+
+}
+
+
+
+/*
+Encapsulation: Binding data members and member methods into single unit via getters
+and setter methods
+
+
+ */
