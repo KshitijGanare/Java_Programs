1973d737f8eb9d98f24a8cd0fbd60135a07b4986
Kshitij Ganare <kshitijganare123@gmail.com>
2025-09-09 23:17:03 +0530

    Abstraction Code

---
+++ src/concepts/oops/Abstraction/AbstractClass/Maruti.java
@@ -0,0 +1,10 @@
+package concepts.oops.Abstraction.AbstractClass;
+
+public class Maruti extends Features {
+
+    @Override
+    public String engine() {
+        return "This vehicle requires engine";
+    }
+
+}
