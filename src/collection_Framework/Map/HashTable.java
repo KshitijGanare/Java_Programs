6a0d0b7fc7f7e0caa2cab1a8ae2b2093dd31c50b
Kshitij Ganare <kshitijganare123@gmail.com>
2026-01-10 22:28:23 +0530

    Map Collection Demo

---
+++ src/collection_Framework/Map/HashTable.java
@@ -0,0 +1,13 @@
+package collection_Framework.Map;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashTable {
+    public static void main(String[] args) {
+
+        Map hashtable = new Hashtable();
+
+
+    }
+}
