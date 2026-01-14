b9bed71aad376745310c0d9589b8b867805ce8a6
Kshitij Ganare <kshitijganare123@gmail.com>
2026-01-14 08:40:23 +0530

    New Program

--- src/DSA/String_Programs/reverseString.java
+++ src/DSA/String_Programs/reverseString.java
@@ -1,25 +1,41 @@
 package DSA.String_Programs;
 
-import java.util.Scanner;
-
 public class reverseString {
     public static void main(String[] args) {
-        byPointer("Selenium");
-        reverseStringBuffer("Selenium");
+        printReverseWithoutSpace("AUTOmation");
+        byPointerWithSpace("Selenium");
+        System.out.println(reverseUsingStringBuffer("automation"));
+
 
     }
 
-    public static void byPointer(String str) {
+
+    public static void printReverseWithoutSpace(String str){
+
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            System.out.print(str.charAt(i));
+        }
+
+        // Time O(n) Space O(1)
+    }
+
+
+    public static void byPointerWithSpace(String str) {
         char[] charArray = str.toCharArray();
         for (int i = charArray.length-1 ; i >= 0 ; i--) {
             System.out.print(charArray[i]);
         }
 
+       // Time O(n)  Space O(n)
     }
 
-    public static void reverseStringBuffer(String str) {
+
+    public static StringBuffer reverseUsingStringBuffer(String str) {
         StringBuffer stringBufferObj = new StringBuffer(str);
-        System.out.print("\n"+stringBufferObj.reverse());
+
+        return stringBufferObj.reverse();
+
+        // Time O(n) Space O(n)
 
     }
 
