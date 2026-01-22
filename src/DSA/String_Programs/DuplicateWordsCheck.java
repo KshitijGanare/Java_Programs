5fe3d250ffd3ad241d7c47563283b61113e0c412
Kshitij Ganare <kshitijganare123@gmail.com>
2026-01-22 07:25:49 +0530

    New Programs added

---
+++ src/DSA/String_Programs/DuplicateWordsCheck.java
@@ -0,0 +1,39 @@
+package DSA.String_Programs;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DuplicateWordsCheck {
+    public static void main(String[] args) {
+
+        duplicateWordCheckByMap("automation");
+    }
+
+    public static void duplicateWordCheckByMap(String word){
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        char[] wordArray = word.toCharArray();
+
+        for (int i = 0; i < wordArray.length; i++) {
+
+            char ch = wordArray[i];
+
+            if (map.containsKey(ch)){
+                map.put(ch, map.get(ch) + 1 );
+            }else {
+                map.put(ch, 1);
+            }
+
+        }
+
+        for (Map.Entry<Character, Integer> printMap: map.entrySet()){
+
+            if (printMap.getValue() > 1){
+                System.out.println(printMap.getKey() + "-->" + printMap.getValue());
+            }
+
+        }
+
+    }
+}
--- src/DSA/String_Programs/ReverseStringPreserveWordOrder.java
+++ src/DSA/String_Programs/ReverseStringPreserveWordOrder.java
@@ -4,11 +4,30 @@ public class ReverseStringPreserveWordOrder {
     public static void main(String[] args) {
     // Reverse characters of each word while keeping word positions intact
         reverseStringCharacters();
+        reverseStringCharactersIgnSpace();
     }
 
     public static void reverseStringCharacters() {
-        String sentence = "Kshitij    is the Best";
+        String sentence = "Kshitij     is  the   Best";
         String[] words = sentence.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+
+            char[] singleWord = words[i].toCharArray();
+
+            for (int j = singleWord.length-1 ; j >= 0 ; j--) {
+                System.out.print(singleWord[j]);
+            }
+
+        }
+    }
+
+
+    public static void reverseStringCharactersIgnSpace() {
+        String sentence = "Kshitij   is  the Best";
+        String[] words = sentence.split("\\s+");
+        System.out.println(words.length);
+        System.out.println(sentence.length());
 
         for (int i = 0; i < words.length; i++) {
 
